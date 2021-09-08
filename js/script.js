6e55048b9c0f233775bc9c8c34c000b09cfab9ee
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-09-08 10:03:07 +0200

    Update script.js
    Created pokemonlist array for Pokedox App

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [
+{name : 'Bulbasur',height : 7,types = ['grass','poison'] },
+{name : 'Weedle',height : 3,types = ['Bug','poison'] },
+{name : 'Pidgey',height : 3,types = ['Flying','Normal'] },
+{name : 'Nidoqueen',height : 1,types = ['ground','poison'] },
+{name : 'Paras',height : 1,types = ['grass','bug'] },
+{name : 'Slowpoke',height : 1,types = ['psychic','water'] }
+]
+ 
