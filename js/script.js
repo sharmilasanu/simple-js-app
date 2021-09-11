741a8ed9d06aabd380f4d16aa5aa9f2b014eec0c
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-09-11 12:47:28 +0200

    Update script.js
    loop to display the pokemon details

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let pokemonList = [
+<<<<<<< Updated upstream
 {name : 'Bulbasur',height : 7,types : ['grass','poison'] },
 {name : 'Charmander',height : 3,types : ['Bug','poison'] },
 {name : 'Ivysur',height : 0.3,types : ['Flying','Normal'] },
@@ -27,3 +28,33 @@ else{
     document.getElementById('pok_height'+ i).innerHTML = pokemonList[i].height
 }
 }
+=======
+    {name : 'Bulbasur',height : 7,types : ['grass','poison'] },
+    {name : 'Charmander',height : 3,types : ['Bug','poison'] },
+    {name : 'Ivysur',height : 0.3,types : ['Flying','Normal'] },
+    {name : 'Kakuna',height : 1,types : ['ground','poison'] },
+    {name : 'Blastoise',height : 1,types : ['grass','bug'] },
+    {name : 'Meganium',height : 1,types : ['psychic','water']},
+    {name : 'Ninetales',height : 1,types : ['ground','poison'] },
+    {name : 'Taillow',height : 1,types : ['grass','bug'] },
+    {name : 'Weedle',height : 1,types : ['psychic','water']},
+    {name : 'Squirtle',height : 1,types : ['ground','poison'] },
+    {name : 'Beautifly',height : 1,types : ['ground','poison'] },
+    {name : 'Torterra',height : 1,types : ['ground','poison'] },
+    {name : 'Vespiquen',height : 4,types : ['grass','bug'] },
+    {name : 'Floatzel',height : 5,types : ['psychic','water']},
+    {name : 'Mismagius',height : 6,types : ['ground','poison'] },
+    {name : 'Magmortar',height : 5,types : ['ground','poison'] },
+    {name : 'Masmoswine',height : 1.2,types : ['flying','bug'] }
+    ]
+    for(let i=0;i<= pokemonList.length;i++){
+    document.getElementById('pok_name'+ i).innerHTML = pokemonList[i].name
+    if(pokemonList[i].height > 3)
+    {
+    document.getElementById('pok_height'+ i).innerHTML = pokemonList[i].height+ ' ' + '(Wow!Thats big)'
+    }
+    else{
+        document.getElementById('pok_height'+ i).innerHTML = pokemonList[i].height
+    }
+    }
+>>>>>>> Stashed changes
