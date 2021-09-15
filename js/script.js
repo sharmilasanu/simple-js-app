0aeaad792e6dbf90bd574dde61431b1f1bb1415f
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-09-15 19:14:02 +0200

    Function loadList will fetch pokemonList from Api

--- js/script.js
+++ js/script.js
@@ -1,26 +1,10 @@
 
 let pokemonRepository = (function(){  
-    let pokemonList = [
-        {name : 'bulbasur',height : 7,types : ['grass','poison'] },
-        {name : 'charmander',height : 3,types : ['Bug','poison'] },
-        {name : 'ivysaur',height : 0.3,types : ['Flying','Normal'] },
-        {name : 'kakuna',height : 1,types : ['ground','poison'] },
-        {name : 'blastoise',height : 1,types : ['grass','bug'] },
-        {name : 'meganium',height : 1,types : ['psychic','water']},
-        {name : 'ninetales',height : 1,types : ['ground','poison'] },
-        {name : 'taillow',height : 1,types : ['grass','bug'] },
-        {name : 'weedle',height : 1,types : ['psychic','water']},
-        {name : 'squirtle',height : 1,types : ['ground','poison'] },
-        {name : 'beautifly',height : 1,types : ['ground','poison'] },
-        {name : 'torterra',height : 1,types : ['ground','poison'] },
-        {name : 'vespiquen',height : 4,types : ['grass','bug'] },
-        {name : 'floatzel',height : 5,types : ['psychic','water']},
-        {name : 'mismagius',height : 6,types : ['ground','poison'] },
-        {name : 'magmortar',height : 5,types : ['ground','poison'] },
-        {name : 'mamoswine',height : 1.2,types : ['flying','bug'] }
-        ]
+    let pokemonList = []
+        let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
     function getAll() {
             return pokemonList;
+            console.log(pokemonList)
           }
     function add(pokemon) 
         {
@@ -33,10 +17,10 @@ let pokemonRepository = (function(){
             let div = document.createElement('div');                  /*creates div element*/
             div.classList.add('grid_item');                          /*adds class div element*/
             grid_layout.appendChild(div)
+
            
             let pokemonimg =  document.createElement("img")
-            pokemonimg.src ="img/"+pokemon.name+".png"
-            div.appendChild(pokemonimg)
+
 
             let listpokemon = document.createElement('li')    
             let button =  document.createElement("button")
@@ -44,27 +28,62 @@ let pokemonRepository = (function(){
             button.classList.add('button-class')
             listpokemon.appendChild(button)
           
-           pokemonimg.appendChild(listpokemon)
+       //    pokemonimg.appendChild(listpokemon)
            div.appendChild(listpokemon)
             
             button.addEventListener('click', function () {
                 
-                showDetails(pokemon)
+                showDetails(pokemon);
+
             })
         }
+        
         function showDetails(pokemon){
         
-            console.log(pokemon.name)
+            loadDetails(pokemon).then(function () {
+                console.log(pokemon);
+              });
         }
+        function loadList(){
+             return  fetch(apiUrl).then(function(response){
+          return  response.json();
+            }).then(function(json){
+              json.results.forEach(function (item){
+              let pokemon = {
+                  name : item.name,
+                  detailsUrl : item.url
+              };
+              add(pokemon);
+              });
+              }).catch(function (error){
+               console.error(error);
+              })
+        }
+        function loadDetails(item) {
+            let url = item.detailsUrl;
+            return  fetch(url).then(function (response) {
+                return  response.json();
+            }).then(function (details) {
+             // Now we add  the details to the item
+              item.imageUrl = details.sprites.front_default;
+              item.height = details.height;
+              item.types = details.types;
+            }).catch(function (error) {
+              console.error(error);
+            });
+          }
         
    return {
        getAll : getAll,
        add : add,
        addListItem : addListItem,
-       showDetails : showDetails
+       showDetails : showDetails,
+       loadList : loadList,
+       loadDetails :  loadDetails
    }
 })();
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon)
-});
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+      pokemonRepository.addListItem(pokemon);
+    });
+  });
